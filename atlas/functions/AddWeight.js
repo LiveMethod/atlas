efc9266b6c86ad1ed1a7c1a74a976f5cda5f3abb
LiveMethod <3277759+LiveMethod@users.noreply.github.com>
2024-01-01 19:56:01 -0800

    datetime

--- atlas/functions/AddWeight.js
+++ atlas/functions/AddWeight.js
@@ -2,7 +2,7 @@ exports = function({ query, headers, body}, response) {
 
   const restoreDynamicDate = (obj) => {
     console.log(JSON.stringify(obj, null, 2));
-    obj['timestamp'] = Date.parse(obj['timestamp']);
+    obj['timestamp'] = new Date(obj['timestamp']);
     console.log(JSON.stringify(obj, null, 2));
     return obj;
   }
