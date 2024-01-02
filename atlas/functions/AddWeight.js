1854ba05e92c2fa980c85979f36dfffc52ca7a21
LiveMethod <3277759+LiveMethod@users.noreply.github.com>
2024-01-01 19:50:19 -0800

    datetime

--- atlas/functions/AddWeight.js
+++ atlas/functions/AddWeight.js
@@ -1,15 +1,17 @@
 exports = function({ query, headers, body}, response) {
-  let parsedBody = JSON.parse(body.text());
-  console.log(parsedBody);
-  parsedBody['timestamp'] = Date.parse(parsedBody['timestamp']);
-  
-  console.log(parsedBody);
+
+  const restoreDynamicDate = (obj) => {
+    console.obj(JSON.stringify(obj, null, 2));
+    obj['timestamp'] = Date.parse(obj['obj']);
+    console.obj(JSON.stringify(obj, null, 2));
+    return obj;
+  }
 
   const result = context.services
     .get('mongodb-atlas')
     .db('data')
     .collection('scale')
-    .insertOne(parsedBody);
+    .insertOne(restoreDynamicDate(JSON.parse(body.text())));
   
-  return parsedBody;
+  return result;
 };
